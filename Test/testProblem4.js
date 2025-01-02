b0cd4f94d6d2d609524f80c4c594b5dafe6acb19
Sivakumar N <shivask94423@gmail.com>
2025-01-02 10:39:06 +0530

    To get name and city of individual at position 3.

---
+++ Test/testProblem4.js
@@ -0,0 +1,8 @@
+// Write a function that accesses and logs the name and city of the individual at the index position 3 in the dataset.
+
+const arrayOfObjects = require('../database.js');
+const positionThree = require('../problem4.js');
+
+const result = positionThree(arrayOfObjects);
+
+console.log(result);
