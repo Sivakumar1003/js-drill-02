5ed7d65ba3e30411ec77b7441c40b0b5f7e0b3be
Sivakumar N <shivask94423@gmail.com>
2025-01-06 17:50:20 +0530

    function to get user whom in third position.

--- problem4.js
+++ problem4.js
@@ -1,5 +1,6 @@
+// function to get user whom in third position.
 function positionThree (arrayOfObjects) {
-    return "name: " + arrayOfObjects[2].name + ", City:" + arrayOfObjects[2].city;
+    return `Name: ${arrayOfObjects[2]["name"]}, City: ${arrayOfObjects[2]["city"]}.`;
 }
 
 module.exports = positionThree;
