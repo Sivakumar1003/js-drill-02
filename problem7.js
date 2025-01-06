5ee465a4136ece0af49b5a4284290e78b8becea3
Sivakumar N <shivask94423@gmail.com>
2025-01-06 17:52:15 +0530

    function to get name and email at particular age.

--- problem7.js
+++ problem7.js
@@ -1,12 +1,10 @@
+//  function to get name and email at particular age.
 function getNameAndEmail(arrayOfObjects, age) {
-    let nameAndEmail = [];
-    for(let details of arrayOfObjects) {
-        if(details.age == age) {
-            let resultName = [details.name];
-            resultName.push(details.email);
-            nameAndEmail.push(resultName);
-        }
-    }
-    return nameAndEmail;
+    return arrayOfObjects.filter(user => {
+        return user["age"] == age;
+    })
+    .map((user) => {
+        return [user["name"], user["email"]]
+    });
 }
 module.exports =  getNameAndEmail;
